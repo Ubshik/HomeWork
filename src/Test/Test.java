9c0914f2d69d4d8ac0ed0baf7ae301195a2de701
ubshik <ubshik@gmail.com>
2016-10-13 20:33:09 +0300

    It must

---
+++ src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+/**Anton Bondarchik
+ * Created by Acer5740 on 12.10.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Don't worry, be happy!");
+        System.out.println("Try be better");
+        System.out.println("Yeap! I did it!");
+    }
+}
+//github.com/Ubshik/HomePage.git
---
+++ src/Test/Test2.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.util.*;
+
+
+/**
+ * Created by Acer5740 on 12.10.2016.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("New idea!");
+        System.out.println("Maybe it is new idea too!");
+    }
+}
